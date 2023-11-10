e798c972f049239e5aa9fcdc0ad07321d985a76e
Ahmed Omer <ahmedomer13218@gmail.com>
2023-11-10 21:41:22 +0200

    adding some up work

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,33 +26,33 @@ function calculateAge(birthDate) {
   return age;
 }
 
-exports.updateMe = catchAsync(async (req, res, next) => {
-  // 1) Create Error if user Posted password data
-  // i dont think it is even good idea -> i wont do it
+// exports.updateMe = catchAsync(async (req, res, next) => {
+//   // 1) Create Error if user Posted password data
+//   // i dont think it is even good idea -> i wont do it
 
-  // 2) filter unwanted fields {banned fields}
-  const filteredBody = filterObj(req.body, 'username', 'email');
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true,
-    runValidators: true,
-  }); // { is the option obj} will run the validator again to check the email and new: true to return the new user data
+//   // 2) filter unwanted fields {banned fields}
+//   const filteredBody = filterObj(req.body, 'username', 'email');
+//   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+//     new: true,
+//     runValidators: true,
+//   }); // { is the option obj} will run the validator again to check the email and new: true to return the new user data
 
-  // 3) sent the responce
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user: updatedUser,
-    },
-  });
-});
+//   // 3) sent the responce
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       user: updatedUser,
+//     },
+//   });
+// });
 
-exports.deleteMe = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, { active: false });
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+// exports.deleteMe = catchAsync(async (req, res, next) => {
+//   await User.findByIdAndUpdate(req.user.id, { active: false });
+//   res.status(204).json({
+//     status: 'success',
+//     data: null,
+//   });
+// });
 
 // MICRO EndPoints
 
