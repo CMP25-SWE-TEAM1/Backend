ceeea4d1eaca9136789f5e08fb21f03cde4e9613
amr-salahuddin <ar.slhdn@gmail.com>
2023-11-26 14:06:27 +0200

    Google Authentication Router

--- google-auth-passport.js
+++ google-auth-passport.js
@@ -4,3 +4,15 @@ const googleAuthPassport = require("passport");
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+        },
+        function (accessToken, refreshToken, profile, done) {
+            done(null, profile);
+        }
+    )
+);
