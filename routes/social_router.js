b743054639ccfe7d90c331b6e1cc62a16b00a095
amr-salahuddin <ar.slhdn@gmail.com>
2023-11-26 13:21:25 +0200

    Google Authentication Router

--- routes/social_router.js
+++ routes/social_router.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
+const dotenv = require("dotenv");
+dotenv.config({ path: './config/dev.env' });
 const passport = require("passport");
+
+const CLIENT_URL = process.env.CLIENT_URL;
+
