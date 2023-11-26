a0a12cd4e02cf1ded3836d7e5654ea1e09b295f1
amr-salahuddin <ar.slhdn@gmail.com>
2023-11-26 13:23:16 +0200

    Google Authentication Router

--- routes/social_router.js
+++ routes/social_router.js
@@ -5,3 +5,13 @@ const passport = require("passport");
 
 const CLIENT_URL = process.env.CLIENT_URL;
 
+
+router.get("/login/success", (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            success: true,
+            message: "successful",
+            user: req.user,
+        });
+    }
+});
